919a497d0568dd3d9d02fba7a51501e17fdff9fe
happymuzhik <ber_kut_91@mail.ru>
2017-09-22 17:28:44 +0300

    same

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
-const init = require('./lib/init');
+const Telegram = require('node-telegram-bot-api');
+const { BOT_TOKEN } = require('./lib/init');
+
+const bot = new Telegram(BOT_TOKEN, {polling:false});
+
+console.log('Everything is fine!');
