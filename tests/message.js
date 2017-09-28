12a37e67435b13e4b4d23b08f0b5e6fb716aaa00
happymuzhik <ber_kut_91@mail.ru>
2017-09-28 15:13:08 +0300

    multiple urls added

--- tests/message.js
+++ tests/message.js
@@ -6,6 +6,8 @@ const test_msg1 = {
     },
     entities: [
         {
+            offset: 0,
+            length: 34,
             type: 'url'
         }
     ],
@@ -18,14 +20,49 @@ const test_msg2 = {
     },
     entities: [
         {
+            offset: 0,
+            length: 36,
             type: 'url'
         }
     ],
     text: 'https://vk.com/wall-140927803_207458'
 };
 
-// createMessage(test_msg1)
-    // .then( res => console.log(res) );
+const test_msg3 ={
+    message_id: 22,
+    chat: {
+        id: -1001097752175,
+        title: 'My films',
+        type: 'channel'
+    },
+    date: 1506518551,
+    text: 'в порядке выхода:\nпервая https://youtu.be/Vn-csFKhQCg\nвторая https://youtu.be/yJYH9aGEEGg\nтретья https://sova.ponominalu.ru/novosti/black-out-2022/',
+    entities:
+     [
+        {
+            offset: 25,
+            length: 28,
+            type: 'url'
+        },
+        {
+            offset: 61,
+            length: 28,
+            type: 'url'
+        },
+        {
+            offset: 97,
+            length: 50,
+            type: 'url'
+         }
+    ]
+    
+}
 
-createMessage(test_msg2)
+// createMessage(null, test_msg1)
+//     .then( res => console.log(res) );
+
+createMessage(null, test_msg2)
     .then( res => console.log(res) );
+
+// createMessage(null, test_msg3)
+    // .then( res => console.log(res) );
